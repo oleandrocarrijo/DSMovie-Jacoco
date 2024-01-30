1c74928e1a0f3a4e001558dc91c25f43e603f2cf
Leandro Carrijo <7leandrobatista@gmail.com>
2024-01-30 15:59:09 -0300

    feat: Find all movies test implemented

--- src/test/java/com/devsuperior/dsmovie/services/MovieServiceTests.java
+++ src/test/java/com/devsuperior/dsmovie/services/MovieServiceTests.java
@@ -1,18 +1,61 @@
 package com.devsuperior.dsmovie.services;
 
+import com.devsuperior.dsmovie.dto.MovieDTO;
+import com.devsuperior.dsmovie.entities.MovieEntity;
+import com.devsuperior.dsmovie.repositories.MovieRepository;
+import com.devsuperior.dsmovie.tests.MovieFactory;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.Mockito;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageImpl;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
+
+import java.util.List;
+
+import static org.mockito.ArgumentMatchers.any;
 
 @ExtendWith(SpringExtension.class)
 public class MovieServiceTests {
 	
 	@InjectMocks
 	private MovieService service;
-	
+	@Mock
+	private MovieRepository repository;
+
+	private String movieName;
+	private PageImpl<MovieEntity> page;
+	private MovieEntity movie;
+	private MovieDTO movieDTO;
+
+	@BeforeEach
+	void setUp() throws Exception{
+		movieName = "Test Movie";
+
+		movie = MovieFactory.createMovieEntity(movieName);
+		movieDTO = new MovieDTO(movie);
+		page = new PageImpl<>(List.of(movie));
+
+		Mockito.when(repository.searchByTitle(any(), (Pageable) any())).thenReturn(page);
+	}
+
 	@Test
 	public void findAllShouldReturnPagedMovieDTO() {
+
+		Pageable pageable = PageRequest.of(0, 12);
+		String name = "Thor";
+
+		Page<MovieDTO> result = service.findAll(name, pageable);
+
+		Assertions.assertNotNull(result);
+		Assertions.assertEquals(result.getSize(), 1);
+		Assertions.assertEquals(result.iterator().next().getTitle(), movieName);
 	}
 	
 	@Test
--- src/test/java/com/devsuperior/dsmovie/tests/MovieFactory.java
+++ src/test/java/com/devsuperior/dsmovie/tests/MovieFactory.java
@@ -9,6 +9,11 @@ public class MovieFactory {
 		MovieEntity movie = new MovieEntity(1L, "Test Movie", 0.0, 0, "https://www.themoviedb.org/t/p/w533_and_h300_bestv2/jBJWaqoSCiARWtfV0GlqHrcdidd.jpg");
 		return movie;
 	}
+
+	public static MovieEntity createMovieEntity(String title) {
+		MovieEntity movie = new MovieEntity(1L, title, 0.0, 0, "https://www.themoviedb.org/t/p/w533_and_h300_bestv2/jBJWaqoSCiARWtfV0GlqHrcdidd.jpg");
+		return movie;
+	}
 	
 	public static MovieDTO createMovieDTO() {
 		MovieEntity movie = createMovieEntity();
